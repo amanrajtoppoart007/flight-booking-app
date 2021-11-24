52f262d227d8a32aa833e3d6b38c833eaf580478
Mazahir <Mazahirnajmi26@gmail.com>
2021-11-24 20:04:42 +0530

    Update files

--- src/navigation/HotelStackNavigator.js
+++ src/navigation/HotelStackNavigator.js
@@ -13,11 +13,11 @@ import GuestReviews from '../screens/Hotel/GuestReviews';
 const Stack = createStackNavigator();
 const HotelStackNavigator = () => (
   <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Screen name={'Home'} component={Home} />
     <Stack.Screen name={'HotelDetails'} component={HotelDetails} />
     <Stack.Screen name={'GuestReviews'} component={GuestReviews} />
     <Stack.Screen name={'GuestDetails'} component={GuestDetails} />
     <Stack.Screen name={'Payment'} component={Payment} />
-    <Stack.Screen name={'Home'} component={Home} />
     <Stack.Screen name={'BookingStatus'} component={BookingStatus} />
     <Stack.Screen name={'Checkout'} component={Checkout} />
     <Stack.Screen name={'SearchResult'} component={SearchResult} />
