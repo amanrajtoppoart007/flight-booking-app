b3f758bb88f302591adb524539761032f1e7faae
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-11-30 00:46:49 +0530

    Update Accordion.js

--- src/components/Flight/ReviewItinerary/Accordion.js
+++ src/components/Flight/ReviewItinerary/Accordion.js
@@ -1,8 +1,7 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {ListItem} from 'react-native-elements';
+import {Icon} from 'react-native-elements';
 import Font from '../../../layout/Font';
-import Colors from '../../../layout/Colors';
 import commonStyle from '../../../layout/Style';
 import DepartureSvg from '../../Svg/Departure.svg';
 
@@ -11,58 +10,43 @@ function Accordion({title, itinerary, Content, isCollapsed}) {
 
   return (
     <View>
-      <ListItem.Accordion
-        containerStyle={styles.container}
-        icon={{
-          name: 'plus',
-          type: 'font-awesome',
-          size: 20,
-          color: Colors.secondary,
-        }}
-        expandIcon={{
-          name: 'minus',
-          type: 'font-awesome',
-          size: 20,
-          color: Colors.secondary,
-        }}
-        content={
-          <>
-            <ListItem.Content>
+      <View style={styles.card}>
+        <View style={commonStyle.rowSpaceBetween}>
+          <View style={commonStyle.rowSpaceBetween}>
+            <View style={commonStyle.rowCenter}>
               <View>
-                <View
-                  style={[
-                    commonStyle.marginVertical(5),
-                    commonStyle.rowFlexStart,
-                  ]}>
-                  <View style={commonStyle.rowCenter}>
-                    <View>
-                      <DepartureSvg />
-                    </View>
-                    <View style={commonStyle.marginHorizontal(8)}>
-                      <Text style={styles.title}>{title}</Text>
-                    </View>
-                  </View>
-                  <View>
-                    <Text style={styles.itinerary}>{itinerary}</Text>
-                  </View>
-                </View>
+                <DepartureSvg />
               </View>
-            </ListItem.Content>
-          </>
-        }
-        isExpanded={expanded}
-        onPress={() => {
-          setExpanded(!expanded);
-        }}>
-        {expanded && Content}
-      </ListItem.Accordion>
+              <View style={commonStyle.marginHorizontal(8)}>
+                <Text style={styles.title}>{title}</Text>
+              </View>
+            </View>
+            <View>
+              <Text style={styles.itinerary}>{itinerary}</Text>
+            </View>
+          </View>
+          <View style={commonStyle.marginHorizontal(15)}>
+            <Icon
+              onPress={() => setExpanded(!expanded)}
+              name={expanded ? 'minus' : 'plus'}
+              type={'font-awesome'}
+              size={18}
+              color={'#26698E'}
+            />
+          </View>
+        </View>
+      </View>
+      <>{expanded && <View>{Content && Content}</View>}</>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  card: {
+    height: 50,
     backgroundColor: 'rgba(157,196,241,0.16)',
+    justifyContent: 'center',
+    paddingHorizontal: 15,
   },
   title: {
     fontFamily: Font.AvenirMedium,
