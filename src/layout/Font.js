05c88f286c5d421ce4d06fcecf8889fde795b8f8
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-12-07 01:59:50 +0530

    Update Font.js

--- src/layout/Font.js
+++ src/layout/Font.js
@@ -2,8 +2,6 @@ export default {
   AvenirBlack: 'Avenir-Black',
   AvenirMedium: 'Avenir-Medium',
   AvenirBook: 'Avenir-Book',
-  AvenirRegular: 'Avenir-Regular',
   AvenirHeavy: 'Avenir-Heavy',
-  AvenirLight: 'Avenir-Light',
   AvenirRoman: 'Avenir-Roman',
 };
