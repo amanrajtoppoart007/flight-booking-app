4684bdc5923ea39afa67e84158b27210e99a0e02
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-12-08 19:48:53 +0530

    Update FrequentFlyer.js

--- src/components/Flight/Checkout/ExtraSection/FrequentFlyer.js
+++ src/components/Flight/Checkout/ExtraSection/FrequentFlyer.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import Colors from '../../../../layout/Colors';
 import Font from '../../../../layout/Font';
+import commonStyle from '../../../../layout/Style';
+import SelectDropdown from 'react-native-select-dropdown';
+import {Icon, Input} from 'react-native-elements';
+import CalenderSvg from '../../../Svg/Calender.svg';
 
 function FrequentFlyer() {
   const airLines = [
@@ -14,21 +18,75 @@ function FrequentFlyer() {
       value: 'qr',
     },
   ];
-  return <View />;
+  return (
+    <View>
+      <View style={commonStyle.rowFlexStart}>
+        <View>
+          <SelectDropdown
+            defaultValueByIndex={0}
+            defaultButtonText={'Special Assistance'}
+            buttonStyle={styles.dropDownButtonStyle}
+            buttonTextStyle={styles.dropDownButtonTextStyle}
+            renderDropdownIcon={() => {
+              return (
+                <Icon
+                  type={'font-awesome'}
+                  name="chevron-down"
+                  color={'#898989'}
+                  size={14}
+                />
+              );
+            }}
+            rowTextStyle={styles.dropDownButtonTextStyle}
+            renderCustomizedButtonChild={item => {
+              return (
+                <View>
+                  <Text style={styles.dropDownButtonTextStyle}>
+                    {item?.title}
+                  </Text>
+                </View>
+              );
+            }}
+            data={airLines}
+            onSelect={selectedItem => {}}
+            buttonTextAfterSelection={selectedItem => {
+              return selectedItem?.title;
+            }}
+            rowTextForSelection={item => {
+              return item?.title;
+            }}
+          />
+        </View>
+        <View>
+          <TextInput
+            placeholder={'Frequent Flyer Number'}
+            inputContainerStyle={styles.inputContainerStyle}
+            inputStyle={styles.inputTextStyle}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   dropDownButtonStyle: {
-    width: '100%',
+    width: 100,
     backgroundColor: Colors.white,
-    borderWidth: 1,
-    borderColor: '#C3C3C3',
-    borderRadius: 4,
   },
   dropDownButtonTextStyle: {
     fontFamily: Font.AvenirMedium,
     fontSize: 14,
     color: '#6C6C6C',
+  },
+  inputContainerStyle: {
+    width: 200,
+    backgroundColor: Colors.google,
+  },
+  inputTextStyle: {
+    fontFamily: Font.AvenirMedium,
+    fontSize: 14,
+    color: Colors.black,
   },
 });
 
